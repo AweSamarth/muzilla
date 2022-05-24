9d2a2103c7b4668e6af87d207d6b20197492ded1
Samarth Saxena <SamarthSaxena1672003@gmail.com>
2022-05-24 23:57:01 +0530

    will make more changes tomorrow

--- hardhat-tutorial/contracts/NFTContract.sol
+++ hardhat-tutorial/contracts/NFTContract.sol
@@ -3,14 +3,18 @@ pragma solidity ^0.8.14;
 import "hardhat/console.sol";
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract nftContract is ERC1155 {
-    uint256 public constant newtrack=0;
+contract nftContract is ERC1155, Ownable{
+    uint256 public constant newtrack=0; 
 
     constructor() ERC1155("https://bafybeic4vuwhhnl6ipohbc7ipr4cvyqjqnt2dwzz5qt4grkqnn4xixgaym.ipfs.nftstorage.link/") {
 
     }
 
+    function setURI(string memory newuri) public onlyOwner{
+        _setURI(newuri);
+    }
     function mint() public  payable{
         _mint(msg.sender, newtrack, 1, "Congratulations!");
     }
