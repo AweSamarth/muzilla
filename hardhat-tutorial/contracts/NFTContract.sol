1376de002bbaf4c37b7e096f26ffcc743789c2a8
Samarth Saxena <SamarthSaxena1672003@gmail.com>
2022-05-26 18:29:27 +0530

    on polygon now

--- hardhat-tutorial/contracts/NFTContract.sol
+++ hardhat-tutorial/contracts/NFTContract.sol
@@ -12,11 +12,7 @@ contract nftContract is ERC1155, Ownable{
     uint256 public constant newtrack=1; 
 
     constructor() ERC1155("https://ipfs.io/ipfs://bafybeicxxqisagyxmeasorot6raxlesabuw7qviwacogvcnfzukqa2t324/{id}.json") {
-        //this needs to be edited. as this is just a link to the music file uploaded to ipfs and filecoin via nftstorage
-        // The uri needs to be here. the metadata of that should contain this info:
-        /*Song: Arcando - In My Head (feat. Britt Lari) [NCS Release]
-        Music provided by NoCopyrightSounds
-        link: http://NCS.io/InMyHead*/
+
     }
 
     function setURI(string memory newuri) public onlyOwner{
@@ -35,7 +31,8 @@ contract nftContract is ERC1155, Ownable{
     }
 
 
-//address of the smart contract: 0x8F788e5F71e007d3eD92E1e8Aff7bbDC32BE9507
+//address of the smart contract: 0x700663B92515B32F4CCE819BE40607ED691Bcb54
+//it's on polygon now
 
 }
 
--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -1,16 +1,45 @@
 require("@nomiclabs/hardhat-waffle");
-require("dotenv").config({ path: ".env" });
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 
-const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
 
-const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY;
+const ALCHEMY_API_KEY_URL= process.env.ALCHEMY_API_KEY_URL;
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
 
+/**
+ */
 module.exports = {
-  solidity: "0.8.14",
+
+  defaultNetwork: "mumbai",
   networks: {
-    rinkeby: {
-      url: ALCHEMY_API_KEY_URL,
-      accounts: [RINKEBY_PRIVATE_KEY],
+    hardhat: {
     },
+    mumbai: {
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [POLYGONSCAN_API_KEY],
+    }
   },
-};
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  },
+  solidity: {
+    version: "0.8.14",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+}
