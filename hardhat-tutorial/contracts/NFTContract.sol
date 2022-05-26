f2047f14debbdb4ccd5ffba3638ed8ae3da52677
Samarth Saxena <SamarthSaxena1672003@gmail.com>
2022-05-27 03:13:21 +0530

    contract address changed (check the end of the contract), uri now working

--- hardhat-tutorial/contracts/NFTContract.sol
+++ hardhat-tutorial/contracts/NFTContract.sol
@@ -11,7 +11,7 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 contract nftContract is ERC1155, Ownable{
     uint256 public constant newtrack=1; 
 
-    constructor() ERC1155("https://ipfs.io/ipfs://bafybeicxxqisagyxmeasorot6raxlesabuw7qviwacogvcnfzukqa2t324/{id}.json") {
+    constructor() ERC1155("https://ipfs.io/ipfs//bafybeie3dw3vbn44o67szv4zcsxj7iwwvfutwldy4aqvvjdqhc4yk2mogu/{id}.json") {
 
     }
 
@@ -24,14 +24,15 @@ contract nftContract is ERC1155, Ownable{
     function uri(uint256 _tokenid) override public pure returns (string memory) {
         return string(
             abi.encodePacked(
-                "https://ipfs.io/ipfs://bafybeicxxqisagyxmeasorot6raxlesabuw7qviwacogvcnfzukqa2t324/",
+                "https://ipfs.io/ipfs/bafybeie3dw3vbn44o67szv4zcsxj7iwwvfutwldy4aqvvjdqhc4yk2mogu/",
+            
                 Strings.toString(_tokenid),".json"
             )
         );
     }
 
 
-//address of the smart contract: 0x700663B92515B32F4CCE819BE40607ED691Bcb54
+//address of the smart contract: 0x67106EaCAf99c93DB14921b9577098eB24369592
 //it's on polygon now
 
 }
